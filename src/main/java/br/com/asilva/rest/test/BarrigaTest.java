fde0647b0921c9763bcd0e9c1c6efcf98999954a
Anderson de Sa <asilvadesa@gmail.com>
2020-12-07 22:00:44 -0400

    Deve incluir conta com sucesso

--- src/main/java/br/com/asilva/rest/test/BarrigaTest.java
+++ src/main/java/br/com/asilva/rest/test/BarrigaTest.java
@@ -4,6 +4,9 @@ import br.com.asilva.rest.core.BaseTest;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 
 public class BarrigaTest extends BaseTest {
@@ -17,4 +20,36 @@ public class BarrigaTest extends BaseTest {
                 .statusCode(401)
         ;
     }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+        Map<String,String> login = new HashMap<String, String>();
+        login.put("email", "anderson@silva");
+        login.put("senha", "123456");
+
+
+        String token = given()
+                .body(login)
+        .when()
+                .post("/signin")
+        .then()
+                .statusCode(200)
+                .extract().path("token");
+
+
+        Map<String,String> conta = new HashMap<String, String>();
+        conta.put("nome", "CONTA QUALQUER");
+        given()
+                .header("Authorization", "JWT " + token)
+                .body(conta)
+        .when()
+                .post("/contas")
+        .then()
+            .statusCode(201);
+
+
+
+
+    }
+
 }
